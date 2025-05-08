7cdc857d7a25281525e7a056f51db5e127b00fd4
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2025-05-08 13:51:52 +0300

    Add files via upload

--- THERE4U/script.js
+++ THERE4U/script.js
@@ -1,3 +1,34 @@
-document.getElementById('LogIn').addEventListener("click", function () {
-    window.location.href="LogIn.html";
-})
+const logInBtn = document.getElementById('LogIn');
+if (logInBtn) {
+    logInBtn.addEventListener("click", function () {
+        window.location.href = "LogIn.html";
+    });
+}
+
+const createAccountBtn = document.getElementById('CreateAccount');
+if (createAccountBtn) {
+    createAccountBtn.addEventListener("click", function () {
+        window.location.href = "CreateAccount.html";
+    });
+}
+
+const aboutUsBtn = document.getElementById('AboutUs');
+    if(aboutUsBtn) {
+    aboutUsBtn.addEventListener("click", function () {
+        window.location.href = "AboutUs.html";
+    });
+}
+
+const doneeBtn = document.getElementById('donee');
+if (doneeBtn) {
+    doneeBtn.addEventListener("click", function () {
+        window.location.href = "Donee.html";
+    });
+}
+
+const contributorBtn = document.getElementById('contributor');
+if (contributorBtn) {
+    contributorBtn.addEventListener("click", function () {
+        window.location.href = "Contributor.html";
+    });
+}
